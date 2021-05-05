dd468452cb795a68994da2d7367de0f286309326
Ronan <ronan.moriarty@justeattakeaway.com>
2021-05-05 08:04:36 +0100

    Add random selection

--- day-13/script.js
+++ day-13/script.js
@@ -4,6 +4,13 @@ textarea.focus();
 
 textarea.addEventListener('keyup', (e) => {
     createTags(e.target.value);
+    if(e.key === 'Enter') {
+        setTimeout(() => {
+            e.target.value = ''
+        }, 100);
+
+        randomSelect();
+    }
 });
 
 function createTags(input) {
@@ -16,4 +23,38 @@ function createTags(input) {
         tagEl.innerText = tag;
         tagsEl.appendChild(tagEl);
     });
+}
+
+function randomSelect() {
+    const times = 10;
+
+    const interval = setInterval(() => {
+        const tag = pickRandomTag();
+        highlightTag(tag);
+        setTimeout(() => {
+            unhighlightTag(tag);
+        }, 100);
+    }, 100);
+
+    setTimeout(() => {
+        clearInterval(interval);
+
+        const tag = pickRandomTag();
+        setTimeout(() => {
+            highlightTag(tag);
+        }, 100);
+    }, times * 100);
+}
+
+function pickRandomTag() {
+    const tags = document.querySelectorAll('.tag');
+    return tags[Math.floor(Math.random() * tags.length)];
+}
+
+function highlightTag(tag) {
+    tag.classList.add('highlight');
+}
+
+function unhighlightTag(tag) {
+    tag.classList.remove('highlight');
 }
