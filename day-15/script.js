e510ef50bc7a032c160ab6a991f5bf4df8177802
Ronan <ronan.moriarty@justeattakeaway.com>
2021-05-07 08:31:54 +0100

    Follow along with tutorial

---
+++ day-15/script.js
@@ -0,0 +1,28 @@
+const counters = document.querySelectorAll('.counter');
+const targets = counters.forEach(counter => {
+    counter.innerText = '0';
+
+    const updateCounter = () => {
+        const target = +counter.getAttribute('data-target');
+        const c = +counter.innerText;
+        const increment = target / 200;
+        console.log(increment);
+
+        if(c < target) {
+            counter.innerText = `${Math.ceil(c + increment)}`;
+            setTimeout(updateCounter, 1);
+        } else {
+            counter.innerText = target;
+        }
+    };
+
+    updateCounter();
+});
+
+// window.addEventListener('onload', () => {
+//     setInterval(() => {
+//         counters.forEach(counter => {
+
+//         });
+//     }, 100);
+// });
