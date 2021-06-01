cc47ec945e1122138df021d4451b5dd0f1bb3aef
Ronan <ronan.moriarty@justeattakeaway.com>
2021-06-01 18:28:38 +0100

    Follow along with tutorial

---
+++ day-22/script.js
@@ -0,0 +1,81 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+const increase = document.getElementById('increase');
+const decrease = document.getElementById('decrease');
+const colorPicker = document.getElementById('color');
+const clear = document.getElementById('clear');
+const sizeSpan = document.getElementById('size');
+
+let size = 10;
+let color = 'black';
+let x;
+let y;
+let isPressed = false;
+
+canvas.addEventListener('mousedown', (e) => {
+    isPressed = true;
+    x = e.offsetX;
+    y = e.offsetY;
+});
+
+
+canvas.addEventListener('mouseup', (e) => {
+    isPressed = false;
+    x = undefined;
+    y = undefined;
+});
+
+canvas.addEventListener('mousemove', (e) => {
+    if(isPressed) {
+        x2 = e.offsetX;
+        y2 = e.offsetY;
+        drawCircle(x2, y2);
+        drawLine(x, y, x2, y2);
+        x = x2;
+        y = y2;
+    }
+});
+
+increase.addEventListener('click', () => {
+    console.log('increase');
+    size+=5;
+    if(size >= 50) {
+        size = 50;
+    }
+
+    sizeSpan.innerText = size;
+});
+
+decrease.addEventListener('click', () => {
+    console.log('decrease');
+    size-=5;
+    if(size <= 5) {
+        size = 5;
+    }
+
+    sizeSpan.innerText = size;
+});
+
+colorPicker.addEventListener('change', (e) => {
+    color = e.target.value;
+});
+
+clear.addEventListener('click', (e) => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+function drawCircle(x, y) {
+    ctx.beginPath();
+    ctx.arc(x, y, size, 0, Math.PI*2);
+    ctx.fillStyle = color;
+    ctx.fill();
+}
+
+function drawLine(x1, y1, x2, y2) {
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.strokeStyle = color;
+    ctx.lineWidth = size * 2;
+    ctx.stroke();
+}
