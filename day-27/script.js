37487515825e932899642d5fd49d83143c2aaed0
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2021-06-10 22:43:09 +0100

    Day 27 - Toast Notifications (#28)
    * Add project starter files
    
    * Follow along with tutorial
    
    Co-authored-by: Ronan <ronan.moriarty@justeattakeaway.com>

---
+++ day-27/script.js
@@ -0,0 +1,36 @@
+const button = document.getElementById('button');
+const toasts = document.getElementById('toasts');
+
+const messages = [
+    "Message One",
+    "Message Two",
+    "Message Three",
+    "Message Four"
+];
+
+const types = [
+    "info",
+    "success",
+    "error"
+]
+
+button.addEventListener('click', () => createNotification());
+
+function createNotification(message = null, type = null){
+    const notification = document.createElement('div');
+    notification.classList.add('toast');
+    notification.classList.add(type ?? getRandomType());
+    notification.innerText = message ?? getRandomNotification();
+    toasts.appendChild(notification);
+    setTimeout(() => {
+        notification.remove();
+    }, 3000);
+}
+
+function getRandomNotification(){
+    return messages[Math.floor(Math.random() * messages.length)];
+}
+
+function getRandomType(){
+    return types[Math.floor(Math.random() * types.length)];
+}
