952125c99664f3553b04e878b113230b06572f69
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2021-06-19 13:36:23 +0100

    Day 37 - Pokedex (#38)
    * Add project starter files
    
    * Follow along with tutorial
    
    Co-authored-by: Ronan <ronan.moriarty@justeattakeaway.com>

---
+++ day-37/script.js
@@ -0,0 +1,57 @@
+const pokeContainer = document.getElementById('poke-container');
+const POKEMON_COUNT = 150;
+const colours = {
+    fire: '#FDDFDF',
+    grass: '#DEFDE0',
+	electric: '#FCF7DE',
+	water: '#DEF3FD',
+	ground: '#f4e7da',
+	rock: '#d5d5d4',
+	fairy: '#fceaff',
+	poison: '#98d7a5',
+	bug: '#f8d5a3',
+	dragon: '#97b3e6',
+	psychic: '#eaeda1',
+	flying: '#F5F5F5',
+	fighting: '#E6E0D4',
+	normal: '#F5F5F5'
+};
+
+const mainTypes = Object.keys(colours);
+console.log(mainTypes);
+
+const fetchPokemons = async () => {
+    for (let i = 1; i <= POKEMON_COUNT; i++) {
+        await getPokemon(i);
+    }
+};
+
+const getPokemon = async (id) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    createPokemonCard(data);
+}
+
+const createPokemonCard = (pokemon) => {
+    const pokemonElement = document.createElement('div');
+    pokemonElement.classList.add('pokemon');
+    const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+    const id = pokemon.id.toString().padStart(3, '0');
+    const types = pokemon.types.map(type => type.type.name);
+    const type = mainTypes.find(type => types.indexOf(type) > -1);
+    const colour = colours[type];
+    pokemonElement.style.backgroundColor = colour;
+    const pokemonElementInnerHTML = `<div class="image-container">
+    <img src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png" alt="">
+  </div>
+  <div class="info">
+     <span class="number">#${id}</span>
+     <h3 class="name">${name}</h3>
+     <small class="type">Type: <span>${type}</span></small>
+  </div>`;
+    pokemonElement.innerHTML = pokemonElementInnerHTML;
+    pokeContainer.appendChild(pokemonElement);
+};
+
+fetchPokemons();
